c793748c1e750c926899f7f248ff99aef9648773
briptimus <brituor@gmail.com>
2014-11-30 09:27:57 -0800

    solved notes exercise

---
+++ exercises/chapters07-14/data.js
@@ -0,0 +1,62 @@
+var globalData = {
+
+  "2015/03/10" :    
+  {
+    createdate : "2015/03/10",
+
+    editdate : "yyyy/mm/dd",
+
+    text : "1. Create a web app that allows a user to create notes."
+  },
+
+  "2015/02/22" :
+  {
+    createdate : "2015/02/22",
+
+    editdate : "yyyy/mm/dd",
+
+    text : "Ut labore et dolore magna aliqua. Sed do eiusmod tempor incididunt eu fugiat nulla pariatur. Velit esse cillum dolore ullamco laboris nisi ut enim ad minim veniam Ut aliquip ex ea commodo consequat. Cupidatat non proident, sunt in culpa sed do eiusmod tempor incididunt. Excepteur sint occaecat mollit anim id est laborum. Ut labore et dolore magna aliqua. Quis nostrud exercitation velit esse cillum dolore ut enim ad minim veniam. Ut labore et dolore magna aliqua. Sed do eiusmod tempor incididunt eu fugiat nulla pariatur. Velit esse cillum dolore ullamco laboris nisi ut enim ad minim veniam. Ut aliquip ex ea commodo consequat. Cupidatat non proident, sunt in culpa sed do eiusmod tempor incididunt. Excepteur sint occaecat mollit anim id est laborum. Ut labore et dolore magna aliqua. Quis nostrud exercitation velit esse cillum dolore ut enim ad minim veniam."
+  },  
+
+  "2014/12/29" :
+  {
+    createdate : "2014/12/29",
+
+    editdate : "yyyy/mm/dd",
+
+    text : "Ut labore et dolore magna aliqua. Sed do eiusmod tempor incididunt eu fugiat nulla pariatur. Velit esse cillum dolore ullamco laboris nisi ut enim ad minim veniam Ut aliquip ex ea commodo consequat."
+  },
+
+
+  "2014/12/17" :
+  {
+    createdate : "2014/12/17",
+
+    editdate : "yyyy/mm/dd",  
+
+    text : "Ut labore et dolore magna aliqua. Sed do eiusmod tempor incididunt eu fugiat nulla pariatur. Velit esse cillum dolore ullamco laboris nisi ut enim ad minim veniam Ut aliquip ex ea commodo consequat. Cupidatat non proident, sunt in culpa sed do eiusmod tempor incididunt. Excepteur sint occaecat mollit anim id est laborum. Ut labore et dolore magna aliqua. Quis nostrud exercitation velit esse cillum dolore ut enim ad minim veniam. Ut labore et dolore magna aliqua. Sed do eiusmod tempor incididunt eu fugiat nulla pariatur. Velit esse cillum dolore ullamco laboris nisi ut enim ad minim veniam. Ut aliquip ex ea commodo consequat. Cupidatat non proident, sunt in culpa sed do eiusmod tempor incididunt. Excepteur sint occaecat mollit anim id est laborum. Ut labore et dolore magna aliqua. Quis nostrud exercitation velit esse cillum dolore ut enim ad minim veniam"
+  },
+
+  "2014/11/29" :
+  {
+    createdate : "2014/11/29",
+
+    editdate : "yyyy/mm/dd",
+
+    text : "Ut labore et dolore magna aliqua. Sed do eiusmod tempor incididunt eu fugiat nulla pariatur. Velit esse cillum dolore ullamco laboris nisi ut enim ad minim veniam Ut aliquip ex ea commodo consequat."
+  },  
+
+  "2014/11/15" :
+  {
+    createdate : "2014/11/15",
+
+    editdate : "yyyy/mm/dd",    
+
+    text : "Ut labore et dolore magna aliqua. Sed do eiusmod tempor incididunt eu fugiat nulla pariatur. Velit esse cillum dolore ullamco laboris nisi ut enim ad minim veniam Ut aliquip ex ea commodo consequat."
+  }        
+
+};
+
+
+
+
